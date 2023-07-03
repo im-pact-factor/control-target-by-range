b1af5901a21c04a87b3be8b177778e446e509e72
impact <Impact@fanruan.com>
2023-07-03 20:56:51 +0800

    feat: 使用滑块控制对象变化

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
 const range = document.getElementById("range") as HTMLInputElement;
 const box = document.querySelector(".box") as HTMLDivElement;
+const container = document.querySelector(".container") as HTMLDivElement;
+
+const updateBoxStyle = (event: Event) => {
+  const { value } = event.target as HTMLInputElement;
+  const numericValue = parseInt(value);
+  const left = `${
+    (numericValue / 100) * (container.offsetWidth - box.offsetWidth)
+  }px`;
+  const redValue = Math.floor((numericValue / 100) * 255).toString(16);
+  const greenValue = Math.floor(
+    128 + (numericValue / 100) * (255 - 128)
+  ).toString(16);
+  const color = `#${redValue}${greenValue}00`;
+  const borderRadius = `${value}%`;
+
+  box.style.left = left;
+  box.style.backgroundColor = color;
+  box.style.borderRadius = borderRadius;
+};
+
+range.addEventListener("input", updateBoxStyle);
