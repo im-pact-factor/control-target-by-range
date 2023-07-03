a451464aec81844dbab40887799621696548e8db
impact <Impact@fanruan.com>
2023-07-03 21:06:21 +0800

    refactor: 优化下代码

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,45 @@
 const range = document.getElementById("range") as HTMLInputElement;
 const box = document.querySelector(".box") as HTMLDivElement;
 const container = document.querySelector(".container") as HTMLDivElement;
+const MAX_COLOR_VALUE = 255;
+const MID_COLOR_VALUE = 128;
+const PERCENTAGE = 100;
 
 const updateBoxStyle = (event: Event) => {
   const { value } = event.target as HTMLInputElement;
   const numericValue = parseInt(value);
-  const left = `${
-    (numericValue / 100) * (container.offsetWidth - box.offsetWidth)
-  }px`;
-  const redValue = Math.floor((numericValue / 100) * 255).toString(16);
-  const greenValue = Math.floor(
-    128 + (numericValue / 100) * (255 - 128)
-  ).toString(16);
-  const color = `#${redValue}${greenValue}00`;
-  const borderRadius = `${value}%`;
 
-  box.style.left = left;
-  box.style.backgroundColor = color;
-  box.style.borderRadius = borderRadius;
+  const updateBoxPosition = (box: HTMLDivElement, value: number) => {
+    const left = `${
+      (value / 100) * (container.offsetWidth - box.offsetWidth)
+    }px`;
+    box.style.left = left;
+  };
+
+  const updateBoxColor = (box: HTMLDivElement, value: number) => {
+    //初始绿色是：#008000 黄色是：#FFFF00 RGB是红绿蓝
+    //padStart(2, "0")处理下十进制转化为十六进制后只有ABCDEF的特殊情况
+    const redValue = Math.floor((value / PERCENTAGE) * MAX_COLOR_VALUE)
+      .toString(16)
+      .padStart(2, "0");
+    const greenValue = Math.floor(
+      MID_COLOR_VALUE +
+        (value / PERCENTAGE) * (MAX_COLOR_VALUE - MID_COLOR_VALUE)
+    )
+      .toString(16)
+      .padStart(2, "0");
+    const color = `#${redValue}${greenValue}00`;
+    box.style.backgroundColor = color;
+  };
+
+  const updateBoxRadius = (box: HTMLDivElement, value: number) => {
+    const borderRadius = `${value}%`;
+    box.style.borderRadius = borderRadius;
+  };
+
+  updateBoxPosition(box, numericValue);
+  updateBoxColor(box, numericValue);
+  updateBoxRadius(box, numericValue);
 };
 
 range.addEventListener("input", updateBoxStyle);
